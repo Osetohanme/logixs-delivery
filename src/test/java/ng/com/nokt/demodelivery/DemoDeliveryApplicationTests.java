d36ed8a0207c3320b745f9a224a81f59c636c05f
Dawn Chimereze <dawn.chimereze@binghamuni.edu.ng>
2025-03-06 07:37:37 +0100

    Update DemoDeliveryApplicationTests.java

--- src/test/java/ng/com/nokt/demodelivery/DemoDeliveryApplicationTests.java
+++ src/test/java/ng/com/nokt/demodelivery/DemoDeliveryApplicationTests.java
@@ -1,13 +1,1 @@
-package ng.com.nokt.demodelivery;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class DemoDeliveryApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
